c833528ad201a156c2cd4f17b9af1c3be1f6e0d7
Setu Potnis <setupotnis@Setus-MacBook-Pro.local>
2019-07-05 16:41:24 -0400

    added final styling and full search functionality to react app

--- src/recipe.js
+++ src/recipe.js
@@ -1,11 +1,17 @@
 import React from "react"; 
+import style from './recipe.module.css'
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return(
-        <div>
-            <h1>{title}</h1>
-            <p>{calories}</p>
-            <img src={image} alt=""/>
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>Calories: {calories}</p>
+            <img className={style.image} src={image} alt=""/>
         </div>
     );
 }
