c0cf53a367e5c14b4c5d44e053ec7095fed2bc81
Setu Potnis <setupotnis@Setus-MacBook-Pro.local>
2019-07-05 15:41:24 -0400

    added state and useEffect to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
+import recipe from './recipe';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const APP_ID = '9361d0f2'
+  const APP_KEY = "a73dda7b87786a00e1ad98699f14eebe"
+    
+  const [recipes, setRecipes] = useState([])
+    useEffect(() => {
+      getRecipes();
+    }, []);
+
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+      const data = await response.json();
+      setRecipes(data.hits)
+    }
+      return (
+        <div className="App">
+          <form className="search-form">
+            <input className='search-bar' type="text"/>
+            <button className='search-button' type="submit">Search</button>
+          </form>
+        </div>
+      );
+};
 
 export default App;
